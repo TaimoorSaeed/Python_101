9a9e88af55d6bb83e04f0d358c6a0fc240b50536
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-08 20:40:35 +0500

    type conversion

---
+++ 08_type_conversion.py
@@ -0,0 +1,12 @@
+x = 10 # integer
+y = 10.2 # float
+z = "Hello" # string
+
+# Implicit type conversion
+x = x*y 
+print(type(x))
+
+# Explicit type conversion
+age = input("What is your age?")
+age = int(age)
+print(type(age))
