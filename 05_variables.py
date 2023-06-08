a8401915e40ae2e3192a67530b348df401f959f0
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-08 20:40:10 +0500

    variables

---
+++ 05_variables.py
@@ -0,0 +1,29 @@
+# Variables containing special objects
+
+# x = 5 #numeric or integer variable
+# print(x); 
+
+# y = "We are learning python with Ammar"  #string variable
+# print(y);
+
+# x = x + 10; #or x =15
+# print(x);
+
+# #types/class of variables
+# type(x)
+# print(type(x))
+
+# print(type(y))
+
+# Rules to assign a variable
+# 1- the varibale should contain letters, numbers or underscores
+# 2- Donot start with Numbers
+# 3- Spaces are not allowed
+# 4- Do not use keyword used in functions (break, mean, median)
+# 5- Short and descriptive
+# 6- Case sentitivity (Lowercase, upper case letter should be used)
+
+fruits_basket = "Mangoes, Oranges"
+# fruits_basket = 15
+print(fruits_basket)
+print(type(fruits_basket))
