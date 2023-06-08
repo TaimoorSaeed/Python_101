48fac6d2b3f82a4298ff5b5bd6747cc8740d91e8
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-08 20:39:59 +0500

    strings

---
+++ 03_strings.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("we are learning python with ammar")
+
+print("Test for double quote string")
+print('Test for single quote string')
+print(''' test for triple quotes string''')
+
+print("What's up?")
