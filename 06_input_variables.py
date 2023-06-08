c8a9b5ffe0e812199b7892a020c2e8559053972d
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-08 20:40:16 +0500

    input variables

---
+++ 06_input_variables.py
@@ -0,0 +1,16 @@
+# fruits_basket = "Mangoes"
+# print(fruits_basket)
+
+# fruit_basket = input("What is your favourtie fruits?")
+# print(fruit_basket)
+
+# name = input("What is your name? ")
+# greetings = "Hello!"
+# print(greetings,name)
+
+# name = input("What is your name? ")
+# print("Hello!",name)
+
+name = input("What is your name? ")
+age = input("What's your age? ")
+print("Hello!",name, "You are still young")
