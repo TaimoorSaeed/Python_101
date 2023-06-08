92115b4f5de14bd444dbeabb1772b2b07cece482
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-08 20:40:23 +0500

    conditional logic

---
+++ 07_condition_logic.py
@@ -0,0 +1,13 @@
+# Logical operators are either "true or false" or "yes or no" or 0 or 1
+# equal to ==
+# not equal to !=
+# less than <
+# greater than >
+# less than equal to <=
+# greater than equal to >=
+
+age_at_school = 5
+hammad_age = input("how old is hammad?")
+hammad_age = int(hammad_age)
+print(type(hammad_age))
+print(hammad_age == age_at_school)
