94525ca5e870b9506d667fe3d868d3d52a28939a
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-09 02:29:45 +0500

    added functions

---
+++ 10_functions.py
@@ -0,0 +1,39 @@
+#defining a function
+
+print("Codenics")
+
+#1
+# def print_codenics():
+#     print("We are learning with Ammar")
+#     print("We are learning with Ammar")
+#     print("We are learning with Ammar")
+
+# print_codenics()
+
+# def print_codenics(text):
+#     print(text)
+#     print(text)
+#     print(text)
+
+# print_codenics("We are learning python with ammar in codenics youtube channel")
+
+# def school_calcualtor(age,text):
+#     if age == 5:
+#         print("Hammad can join scrool")
+#     elif age>5:
+#         print("Hammad can go to school")
+#     else: 
+#         print("Hammad is still a baby")
+
+
+# school_calcualtor(5,"Hammad")
+
+
+def future_age(age):
+    new_age = age + 20
+    return new_age
+    print(new_age)
+
+
+future_pred_date = future_age(18)
+print(future_pred_date)
