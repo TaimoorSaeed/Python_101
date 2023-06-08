0b1f83211e88c7a18056698c0fb2c08db74e482b
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-09 02:16:17 +0500

    add if else and elif statements

---
+++ 09_If_else_elif.py
@@ -0,0 +1,13 @@
+requied_age_at_school = 3
+hammad_age = 2
+
+# question : can hammad go to school
+
+if hammad_age == requied_age_at_school:
+        print("Hammad can join the school")
+elif hammad_age > requied_age_at_school:
+        print("Hammad can join higher secondar school")
+elif hammad_age == 2:
+        print("You should take care of hammad")
+else: 
+        print("Hammad cannot go to school")
