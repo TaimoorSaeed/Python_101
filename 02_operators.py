b440ab1cca109db5fdb9563f2e11449f67adbe81
TaimoorSaeed <taimoor.dev@gmail.com>
2023-06-08 20:39:56 +0500

    operators

---
+++ 02_operators.py
@@ -0,0 +1,15 @@
+print(2+1)
+print(3-1)
+print(6/2)
+print(2*3)
+print(13%2)
+
+print(6//2) # getting the value on whole number
+
+print(2**3)
+
+#PEDMAS
+# Paranthesis Exponent Multiply Divide Addition Subtraction
+# Left To Right Sequence For Multiplication Division Subtraction and Addition
+
+print(2**3/3**2/2*3/3+6-4)
